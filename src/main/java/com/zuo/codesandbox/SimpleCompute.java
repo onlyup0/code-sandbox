7c30cf05dfe3b3c1efc4e4bb7785a4aaaa1f7806
onlyup0 <139095055+onlyup0@users.noreply.github.com>
2024-10-08 20:51:21 +0800

    Update SimpleCompute.java
    修改

--- src/main/java/com/zuo/codesandbox/SimpleCompute.java
+++ src/main/java/com/zuo/codesandbox/SimpleCompute.java
@@ -6,6 +6,6 @@ public class SimpleCompute {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         System.out.println("结果:" + (a + b));
-        System.out.println("第一次修改");
+        System.out.println("别人第一次修改");
     }
 }
